2018029335b846d16d3e88946c568764e1391760
tsxepo <tsxepo@gmail.com>
2023-04-28 19:39:53 +0200

    set up cors

--- MyInternetSpeed/Program.cs
+++ MyInternetSpeed/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using Models;
 using Services;
 
+var MyAllowedSpecificOrigins = "_myAllowedSpecificOrigins";
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -14,6 +15,16 @@ builder.Services.AddTransient<IStatsRepository, StatsService>();
 builder.Services.Configure<NetworkSpeedDbSettings>(builder.Configuration.GetSection("NetworkSpeedTest"));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options => 
+{
+    options.AddPolicy("default",
+    builder => 
+    {
+        builder.AllowAnyOrigin()
+        .AllowAnyHeader()
+        .AllowAnyMethod();
+    });
+});
 
 var app = builder.Build();
 
@@ -31,7 +42,7 @@ if (app.Environment.IsDevelopment())
 //app.UseHttpsRedirection();
 
 app.UseAuthorization();
-
+app.UseCors("default");
 app.MapControllers();
 
 app.Run();
